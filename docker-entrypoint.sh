793fb25dd82170a0276dd51fd46a13d65f721f44
Mixail132 <millionbaxov@gmail.com>
2024-10-04 14:53:49 +0300

    Remove 'collectstatic' command as unused for now.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,5 @@ python manage.py migrate
 echo "Create a superuser."
 python manage.py createsuperuser-ifnot
 
-echo "Collect static files."
-collectstatic --noinput
-
 echo "Run server."
 python manage.py runserver 0.0.0.0:8000
